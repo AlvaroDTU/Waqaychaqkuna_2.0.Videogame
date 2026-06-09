7a597ba66181267222184cac2fc672ad67d0491e
Aziedot <alvarosteven69@gmail.com>
2026-06-09 01:12:10 -0500

    recursos completos excepto el muirciegalo

--- Waqaychaqkuna_2.0/Recursos.h
+++ Waqaychaqkuna_2.0/Recursos.h
@@ -7,17 +7,27 @@ public ref class Recursos
 public:
     static Bitmap^ guardia = gcnew Bitmap("sprites\\guardia.png");
    
-    //static Bitmap^ reportera = gcnew Bitmap("sprites\\reportera.png");
+    //----------------  ALIADOS  ----------------
+    static Bitmap^ reportera = gcnew Bitmap("sprites\\reportera.png");
+    static Bitmap^ cuidador = gcnew Bitmap("sprites\\cuidador.png");
 
-    //static Bitmap^ huaquero = gcnew Bitmap("sprites\\huaquero.png");
-    //static Bitmap^ manipulador = gcnew Bitmap("sprites\\manipulador.png");
-    //
-    //static Bitmap^ visitante1 = gcnew Bitmap("sprites\\visitante1.png");
-    //static Bitmap^ visitante2 = gcnew Bitmap("sprites\\visitante2.png");
-    //static Bitmap^ visitante3 = gcnew Bitmap("sprites\\visitante3.png");
-    //static Bitmap^ visitante4 = gcnew Bitmap("sprites\\visitante4.png");
-    //static Bitmap^ visitante5 = gcnew Bitmap("sprites\\visitante5.png");
-    //static Bitmap^ visitante6 = gcnew Bitmap("sprites\\visitante6.png");
-    //
-    //static Bitmap^ bat = gcnew Bitmap("sprites\\bat.png");
+    //---------------- VISITANTES ----------------
+    static Bitmap^ visitante1 = gcnew Bitmap("sprites\\visitante1.png");
+    static Bitmap^ visitante2 = gcnew Bitmap("sprites\\visitante2.png");
+    static Bitmap^ visitante3 = gcnew Bitmap("sprites\\visitante3.png");
+    static Bitmap^ visitante4 = gcnew Bitmap("sprites\\visitante4.png");
+
+    //----------------  ENEMIGOS  ----------------
+    static Bitmap^ ladron_brazalete1 = gcnew Bitmap("sprites\\ladron_brazalete1.png");
+    static Bitmap^ ladron_brazalete2 = gcnew Bitmap("sprites\\ladron_brazalete2.png");
+    static Bitmap^ ladron_brazalete3 = gcnew Bitmap("sprites\\ladron_brazalete3.png");
+    static Bitmap^ ladron_brazalete4 = gcnew Bitmap("sprites\\ladron_brazalete4.png");
+    static Bitmap^ ladron_bandana1 = gcnew Bitmap("sprites\\ladron_bandana1.png");
+    static Bitmap^ ladron_bandana2 = gcnew Bitmap("sprites\\ladron_bandana2.png");
+    static Bitmap^ ladron_bandana3 = gcnew Bitmap("sprites\\ladron_bandana3.png");
+    static Bitmap^ ladron_bandana4 = gcnew Bitmap("sprites\\ladron_bandana4.png");
+
+    static Bitmap^ huaquero = gcnew Bitmap("sprites\\huaquero.png");
+
+    static Bitmap^ chavon = gcnew Bitmap("sprites\\chavon.png");
 };
