0511937c2f7faae80119aeebfbae7ef68ce19327
eALEX08 <cobenas2008@gmail.com>
2026-07-03 21:01:45 -0500

    asasdasdasd

--- Waqaychaqkuna_2.0/FrmMenu.h
+++ Waqaychaqkuna_2.0/FrmMenu.h
@@ -101,7 +101,7 @@ namespace Waqaychaqkuna20 {
 		FrmNivel2^ f2 = gcnew FrmNivel2();
 		if (f2->ShowDialog() != System::Windows::Forms::DialogResult::OK)
 		{
-			delete f3;
+			delete f2;
 			return;
 		}
 		delete f2;
