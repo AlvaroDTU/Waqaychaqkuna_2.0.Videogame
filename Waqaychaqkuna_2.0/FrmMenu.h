d1efd350dd0b7fd54f2008b4e96c9ea389e7ee0d
Alexandra Noelia Armas Franco <u20251j464@upc.edu.pe>
2026-07-05 15:23:18 -0500

    menu listo croe

--- Waqaychaqkuna_2.0/FrmMenu.h
+++ Waqaychaqkuna_2.0/FrmMenu.h
@@ -226,13 +226,13 @@ namespace Waqaychaqkuna20 {
         }
 		Void btnJugar_Click(System::Object^ sender, System::EventArgs^ e)
 		{
-			//FrmSlod1^ slods1 = gcnew FrmSlod1();
-			//if (slods1->ShowDialog() != System::Windows::Forms::DialogResult::OK)
-			//{
-			//	delete slods1;
-			//	return;
-			//}
-			//delete slods1;
+			FrmSlod1^ slods1 = gcnew FrmSlod1();
+			if (slods1->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			{
+				delete slods1;
+				return;
+			}
+			delete slods1;
 			FrmNivel1^ f1 = gcnew FrmNivel1();
 			if (f1->ShowDialog() != System::Windows::Forms::DialogResult::OK)
 			{
@@ -244,25 +244,25 @@ namespace Waqaychaqkuna20 {
 			puntaje1 = f1->RetornarPuntaje();
 			
 			delete f1;
-			//
-			//FrmSlod2^ slods2 = gcnew FrmSlod2();
-			//if (slods2->ShowDialog() != System::Windows::Forms::DialogResult::OK)
-			//{
-			//	delete slods2;
-			//	return;
-			//}
-			//delete slods2;
-			//
-			//FrmNivel2^ f2 = gcnew FrmNivel2();
-			//if (f2->ShowDialog() != System::Windows::Forms::DialogResult::OK)
-			//{
-			//	puntaje2 = f2->RetornarPuntaje();
-			//
-			//	delete f2;
-			//	return;
-			//}
-			//puntaje2 = f2->RetornarPuntaje();
-			//delete f2;
+			
+			FrmSlod2^ slods2 = gcnew FrmSlod2();
+			if (slods2->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			{
+				delete slods2;
+				return;
+			}
+			delete slods2;
+			
+			FrmNivel2^ f2 = gcnew FrmNivel2();
+			if (f2->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			{
+				puntaje2 = f2->RetornarPuntaje();
+			
+				delete f2;
+				return;
+			}
+			puntaje2 = f2->RetornarPuntaje();
+			delete f2;
 			
 			puntajeTotal = puntaje1 + puntaje2;
 			
@@ -276,14 +276,14 @@ namespace Waqaychaqkuna20 {
 			FrmNivel3^ f3 = gcnew FrmNivel3(puntajeTotal, txtName->Text);
 			if (f3->ShowDialog() != System::Windows::Forms::DialogResult::OK)
 			{
-				//puntaje3 = f3->RetornarPuntaje();
+				puntaje3 = f3->RetornarPuntaje();
 				delete f3;
 
-				// FrmSlodVictoria^ slodVictoria = gcnew FrmSlodVictoria();
-				// slodVictoria->ShowDialog();
+				 FrmSlodVictoria^ slodVictoria = gcnew FrmSlodVictoria();
+				 slodVictoria->ShowDialog();
 				return;
 			}
-			//puntaje3 = f3->RetornarPuntaje();
+			puntaje3 = f3->RetornarPuntaje();
 			delete f3;
 
 
