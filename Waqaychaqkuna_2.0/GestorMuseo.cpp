9795fe784cd5de5bd41cbcc304606ff18189dfac
Aziedot <alvarosteven69@gmail.com>
2026-07-05 04:00:18 -0500

    kxsl;skl;

--- Waqaychaqkuna_2.0/GestorMuseo.cpp
+++ Waqaychaqkuna_2.0/GestorMuseo.cpp
@@ -30,7 +30,7 @@ void GestorMuseo::crearSprites()
 	frases.push_back("(Interactua usando E con la reportera)");
 	dialogo.iniciar(frases);
 
-	int n1, n2, n3, gx, gy, bat;
+	int n1, n2, n3, gx=1, gy=1, bat;
 	gestor->cargarTodo(n1, n2, n3, gx, gy, bat);
 
 	guardia = new Guardia(gx, gy, 45, 60, 60, 80);
@@ -96,7 +96,6 @@ void GestorMuseo::dibujar(Graphics^ g)
 	for (auto ladron : enemigos) ladron->dibujar(g, escalaX, escalaY);
 	for (auto visitante : visitantes) visitante->dibujar(g, escalaX, escalaY);
 	guardia->dibujar(g, escalaX, escalaY);
-
 	dialogo.dibujar(g, anchoLienzo, altoLienzo);
 }
 
