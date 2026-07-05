630943f52861c8479ddff6d604827cabcc9fb404
eALEX08 <cobenas2008@gmail.com>
2026-07-05 02:11:24 -0500

    fdfdfgdf

--- Waqaychaqkuna_2.0/GestorArchivo.cpp
+++ Waqaychaqkuna_2.0/GestorArchivo.cpp
@@ -1,7 +1,20 @@
 #include "pch.h"
 #include "GestorArchivo.h"
-void GestorArchivo::leerEnemigosMuseo(int& i){}
-void GestorArchivo::leerEnemigosHuaca(int& i){}
-void GestorArchivo::leerEnemigosBiblioteca(int& i){}
-void GestorArchivo::leerGuardia(int& x, int& y){}
-void GestorArchivo::leerBateria(int& i){}
+#include <fstream>
+using namespace std;
+
+void GestorArchivo::cargarTodo(int& n1, int& n2, int& n3, int& gx, int& gy, int& bat) {
+    std::ifstream f("PARAMETERS.txt");
+    if (!f.is_open()) return;
+
+    char sep; 
+
+    f >> n1 >> sep; 
+    f >> n2 >> sep;
+    f >> n3 >> sep; 
+    f >> gx >> sep; 
+    f >> gy >> sep; 
+    f >> bat >> sep;
+
+    f.close();
+}
