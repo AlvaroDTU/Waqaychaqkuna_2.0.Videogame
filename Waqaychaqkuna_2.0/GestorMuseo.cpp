56e47b7b015729193cb77da96734e9a21857bf9b
Aziedot <alvarosteven69@gmail.com>
2026-07-05 04:05:05 -0500

    sadkaslj

--- Waqaychaqkuna_2.0/GestorMuseo.cpp
+++ Waqaychaqkuna_2.0/GestorMuseo.cpp
@@ -30,7 +30,7 @@ void GestorMuseo::crearSprites()
 	frases.push_back("(Interactua usando E con la reportera)");
 	dialogo.iniciar(frases);
 
-	int n1, n2, n3, gx = 1, gy = 1, bat;
+	int n1, n2, n3, gx = 950, gy = 400, bat;
 	gestor->cargarTodo(n1, n2, n3, gx, gy, bat);
 
 	guardia = new Guardia(gx, gy, 45, 60, 60, 80);
