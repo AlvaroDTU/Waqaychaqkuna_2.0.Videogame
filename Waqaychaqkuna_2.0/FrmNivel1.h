10499b42e149a420af7af63b51c1d665b3202dce
Aziedot <alvarosteven69@gmail.com>
2026-07-04 20:54:04 -0500

    OPKSAKAOPSASOPK

--- Waqaychaqkuna_2.0/FrmNivel1.h
+++ Waqaychaqkuna_2.0/FrmNivel1.h
@@ -132,9 +132,10 @@ namespace Waqaychaqkuna20 {
 			   // lblArtilugio1
 			   // 
 			   this->lblArtilugio1->AutoSize = true;
+			   this->lblArtilugio1->BackColor = System::Drawing::Color::Transparent;
 			   this->lblArtilugio1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblArtilugio1->Location = System::Drawing::Point(10, 260);
+			   this->lblArtilugio1->Location = System::Drawing::Point(18, 446);
 			   this->lblArtilugio1->Name = L"lblArtilugio1";
 			   this->lblArtilugio1->Size = System::Drawing::Size(110, 17);
 			   this->lblArtilugio1->TabIndex = 1;
@@ -143,9 +144,10 @@ namespace Waqaychaqkuna20 {
 			   // lblArtilugio2
 			   // 
 			   this->lblArtilugio2->AutoSize = true;
+			   this->lblArtilugio2->BackColor = System::Drawing::Color::Transparent;
 			   this->lblArtilugio2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblArtilugio2->Location = System::Drawing::Point(10, 333);
+			   this->lblArtilugio2->Location = System::Drawing::Point(18, 509);
 			   this->lblArtilugio2->Name = L"lblArtilugio2";
 			   this->lblArtilugio2->Size = System::Drawing::Size(110, 17);
 			   this->lblArtilugio2->TabIndex = 4;
@@ -154,9 +156,10 @@ namespace Waqaychaqkuna20 {
 			   // lblArtilugio3
 			   // 
 			   this->lblArtilugio3->AutoSize = true;
+			   this->lblArtilugio3->BackColor = System::Drawing::Color::Transparent;
 			   this->lblArtilugio3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblArtilugio3->Location = System::Drawing::Point(10, 400);
+			   this->lblArtilugio3->Location = System::Drawing::Point(18, 575);
 			   this->lblArtilugio3->Name = L"lblArtilugio3";
 			   this->lblArtilugio3->Size = System::Drawing::Size(110, 17);
 			   this->lblArtilugio3->TabIndex = 6;
@@ -165,9 +168,10 @@ namespace Waqaychaqkuna20 {
 			   // lblArtilugio4
 			   // 
 			   this->lblArtilugio4->AutoSize = true;
+			   this->lblArtilugio4->BackColor = System::Drawing::Color::Transparent;
 			   this->lblArtilugio4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblArtilugio4->Location = System::Drawing::Point(149, 260);
+			   this->lblArtilugio4->Location = System::Drawing::Point(165, 446);
 			   this->lblArtilugio4->Name = L"lblArtilugio4";
 			   this->lblArtilugio4->Size = System::Drawing::Size(110, 17);
 			   this->lblArtilugio4->TabIndex = 7;
@@ -176,9 +180,10 @@ namespace Waqaychaqkuna20 {
 			   // lblArtilugio5
 			   // 
 			   this->lblArtilugio5->AutoSize = true;
+			   this->lblArtilugio5->BackColor = System::Drawing::Color::Transparent;
 			   this->lblArtilugio5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblArtilugio5->Location = System::Drawing::Point(149, 333);
+			   this->lblArtilugio5->Location = System::Drawing::Point(165, 509);
 			   this->lblArtilugio5->Name = L"lblArtilugio5";
 			   this->lblArtilugio5->Size = System::Drawing::Size(110, 17);
 			   this->lblArtilugio5->TabIndex = 8;
@@ -187,9 +192,10 @@ namespace Waqaychaqkuna20 {
 			   // lblArtilugio6
 			   // 
 			   this->lblArtilugio6->AutoSize = true;
+			   this->lblArtilugio6->BackColor = System::Drawing::Color::Transparent;
 			   this->lblArtilugio6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblArtilugio6->Location = System::Drawing::Point(149, 400);
+			   this->lblArtilugio6->Location = System::Drawing::Point(165, 575);
 			   this->lblArtilugio6->Name = L"lblArtilugio6";
 			   this->lblArtilugio6->Size = System::Drawing::Size(110, 17);
 			   this->lblArtilugio6->TabIndex = 9;
@@ -198,9 +204,10 @@ namespace Waqaychaqkuna20 {
 			   // lblIntentos
 			   // 
 			   this->lblIntentos->AutoSize = true;
+			   this->lblIntentos->BackColor = System::Drawing::Color::Transparent;
 			   this->lblIntentos->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblIntentos->Location = System::Drawing::Point(10, 631);
+			   this->lblIntentos->Location = System::Drawing::Point(6, 700);
 			   this->lblIntentos->Name = L"lblIntentos";
 			   this->lblIntentos->Size = System::Drawing::Size(105, 17);
 			   this->lblIntentos->TabIndex = 5;
@@ -209,9 +216,10 @@ namespace Waqaychaqkuna20 {
 			   // lblDerrotados
 			   // 
 			   this->lblDerrotados->AutoSize = true;
+			   this->lblDerrotados->BackColor = System::Drawing::Color::Transparent;
 			   this->lblDerrotados->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblDerrotados->Location = System::Drawing::Point(10, 661);
+			   this->lblDerrotados->Location = System::Drawing::Point(6, 727);
 			   this->lblDerrotados->Name = L"lblDerrotados";
 			   this->lblDerrotados->Size = System::Drawing::Size(221, 17);
 			   this->lblDerrotados->TabIndex = 3;
@@ -220,9 +228,10 @@ namespace Waqaychaqkuna20 {
 			   // lblPista
 			   // 
 			   this->lblPista->AutoSize = true;
+			   this->lblPista->BackColor = System::Drawing::Color::Transparent;
 			   this->lblPista->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->lblPista->Location = System::Drawing::Point(10, 690);
+			   this->lblPista->Location = System::Drawing::Point(6, 760);
 			   this->lblPista->Name = L"lblPista";
 			   this->lblPista->Size = System::Drawing::Size(122, 17);
 			   this->lblPista->TabIndex = 2;
